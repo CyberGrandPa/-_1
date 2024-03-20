7c7bf415e7038194d7adbf9569d463edd627c889
CyberGrandPa <147988187+CyberGrandPa@users.noreply.github.com>
2024-03-20 20:13:12 +0300

    Delete get_shop_list_by_dishes.py

--- get_shop_list_by_dishes.py
+++
@@ -1,7 +0,0 @@
-quantity_persons = int(input("введите количество персон: "))
-print(f" Список покупок на {quantity_persons} персон:")
-
-
-def get_shop_list_by_dishes(dishes, person_count):
-   shop_dict = {}
-   
