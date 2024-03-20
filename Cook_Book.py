8eba2add244ef36fbf7b0710e1faf55db7dc48b5
CyberGrandPa <147988187+CyberGrandPa@users.noreply.github.com>
2024-03-20 16:10:14 +0300

    Add files via upload

--- Cook_Book.py
+++ Cook_Book.py
@@ -1,13 +1,15 @@
+# 1 часть задания
 def get_cook_book_from_file():
         
     cook_book ={}
 
-    with open (r"E:\питон\ДЗ\чтение и запись в файл\readANDwrite_in_file\recipes.txt", "r", encoding="UTF-8") as f:
+    with open (r"E:\питон\ДЗ\чтение и запись в файл\Cook_Book\recipes.txt", "r", encoding="UTF-8") as f:
     
         for row in f:
+    
             if not row.strip():
                 return
-            dish_name = row.strip() 
+            dish_name =  row.strip() 
             quantity_ingradients = int(f.readline().strip())
             ingradients = []      
             for item in range(quantity_ingradients):
@@ -17,8 +19,28 @@ def get_cook_book_from_file():
                 ingradient_dict['quantity'] = float(ingradient_list[1])
                 ingradient_dict['measure'] = ingradient_list[2]
                 ingradients.append(ingradient_dict)
-            cook_book[dish_name] = ingradients
+            cook_book[dish_name ] = ingradients
             f.readline()
     return cook_book
 cook_book = get_cook_book_from_file()
 print(cook_book)
+print("*" *60)
+
+
+# 2 часть задания
+
+def get_shop_list_by_dishes(dishes):
+    cook_book = get_cook_book_from_file()
+    for dish in dishes:
+        if dish not in cook_book:
+            return
+        shop_list = {}
+        for item in cook_book.values():
+            ingradient_list = {}
+            shop_list.update(item[2])
+           
+    print(shop_list)          
+dish_list = ['Фахитос', 'Омлет']           
+get_shop_list_by_dishes(dish_list)             
+
+    
